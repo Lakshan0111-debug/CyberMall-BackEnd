9bf9498c944601f35fdcdbdeeac9314e91d7a58e
chinthaka Lakshan <diluklakshan01@gmail.com>
2024-10-30 21:41:43 +0530

    firsh commit

---
+++ src/main/java/com/CyberMallBackEnd/CyberMallBackEnd/CyberMallBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.CyberMallBackEnd.CyberMallBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberMallBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberMallBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CyberMallBackEnd/CyberMallBackEnd/CyberMallBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CyberMallBackEnd.CyberMallBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberMallBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
