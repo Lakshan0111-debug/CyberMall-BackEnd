6caceb17c7e2844a9473d16c1bd239c31dfc3093
chinthaka Lakshan <diluklakshan01@gmail.com>
2024-10-31 04:31:48 +0530

    update Product entity

--- src/main/java/com/CyberMallBackEnd/CyberMallBackEnd/entity/Product.java
+++ src/main/java/com/CyberMallBackEnd/CyberMallBackEnd/entity/Product.java
@@ -1,6 +1,33 @@
 package com.CyberMallBackEnd.CyberMallBackEnd.entity;
 
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name = "Product")
+@Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "supplier_name")
+    private String supplierName;
+
+    @Column(name = "unit_price")
+    private String unitPrice;
+
+    @Column(name = "quantity")
+    private String quantity;
+
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
+    private byte[] image;
 }
