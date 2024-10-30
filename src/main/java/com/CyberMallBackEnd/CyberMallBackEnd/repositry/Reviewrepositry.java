6d9a902ee5fdbe4a8801cc030b0d8e50a0920aa7
Hasitha <hasithawijewardhana19@gmail.com>
2024-10-30 12:54:49 -0700

    create repository interface

---
+++ src/main/java/com/CyberMallBackEnd/CyberMallBackEnd/repositry/Reviewrepositry.java
@@ -0,0 +1,7 @@
+package com.CyberMallBackEnd.CyberMallBackEnd.repositry;
+
+import com.CyberMallBackEnd.CyberMallBackEnd.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Reviewrepositry extends JpaRepository<Review,Long> {
+}
